1fe0700e893fbf29c221c779d097d6ae606fdd52
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 20:53:43 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [
+      :players => {
         "Alan Anderson" => {
           :number => 0,
           :shoe => 16,
@@ -57,12 +57,12 @@ def game_hash
           :blocks => 11,
           :slam_dunks => 1,
           }
-          ]
+        }
       },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => [
+      :players => {
         "Jeff Adrien" => {
           :number => 4,
           :shoe => 18,
@@ -113,7 +113,7 @@ def game_hash
           :blocks => 5,
           :slam_dunks => 12,
           }
-          ]
+        }
     }
 }
 end

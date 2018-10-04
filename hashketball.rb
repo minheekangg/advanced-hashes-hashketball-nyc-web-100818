177aad1d09d80e4d1c5449b9833c9cb5abad6e66
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 20:43:06 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ end
 
 
 def num_points_scored(player_name)
-  player_hash = game_hash[:away][:players]["Jeff Adrien"]
+  player_hash = game_hash[:away][:players]["0]
     puts player_hash
 
 end

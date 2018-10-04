43af00ecfbb0e22fc22b3c68a84fb4a3304fbc07
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 20:22:04 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,9 +119,11 @@ end
 
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
-    binding.pry
+    team_data.each do |attribute, data|
+      data.each do |data_item|
+        binding.pry
+      end
+    end
   end
-
-
 
 end

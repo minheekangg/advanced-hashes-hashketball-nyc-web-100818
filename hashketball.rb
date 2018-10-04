ae57b853780f5b72ae6e04dd8e0adc676c4a7b79
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 19:53:59 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ end
 def num_points_scored(player_name)
  game_hash[:home][:players].each do |player, stat|
     if player == player_name
-      puts stat.fetch(:points)
+      puts stat
     end
 end
 end

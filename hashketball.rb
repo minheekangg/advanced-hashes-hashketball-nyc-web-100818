762a2408ffb6b685f9e88cc313d2e4c60efa7463
Min Hee  Park <minheekangg@gmail.com>
2018-10-04 21:35:29 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,10 +156,11 @@ def shoe_size(player_name)
 end
 
 def team_colors(team_name)
+  curr_loc = ""
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if data == team_name
-        curr_loc = location.to_s
+        curr_loc = location
       end
     end
   end
